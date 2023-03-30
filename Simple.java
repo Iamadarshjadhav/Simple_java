bfbac9815206252fc75c81e85e922af2a4ad138e
Adarsh_Jadhav <ajrules14118198@gmail.com>
2023-03-30 12:40:38 +0530

    Second Commit

--- Simple.java
+++ Simple.java
@@ -1,6 +1,8 @@
 public class Simple{
     public static void main(String[]args){
         System.out.println("Hi my name is Adarsh Jadhav");
+        System.out.println("I am from Latur");
+        System.out.println("I have completed my graduation from pune university");
     }
 }
     
